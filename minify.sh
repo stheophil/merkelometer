0fc1e0f1edb9cc01ed8c3882c14ab284c9be75b6
Sebastian Theophil <sebastian@theophil.net>
2014-11-19 22:35:59 +0100

    Update CSS

--- minify.sh
+++ minify.sh
@@ -2,4 +2,4 @@ cd assets/js
 r.js -o name=main.js out=main.min.js baseUrl=.
 
 cd ../..
-uncss index.html > assets/css/main.min.css -i .glyphicon-star 
+uncss -i /.pagination/,.glyphicon-star index.html > assets/css/main.min.css
